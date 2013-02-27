9b667d90da44592b1669fb4f58d40f4812bdbb3d
Neil Renicker <neilrenicker@gmail.com>
2013-02-27 11:53:23 -0500

    create functionality to determine if a number is fizz or buzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,21 @@
-class Fizzbuzz
+class Game
 
   def list
     [1..100]
+  end
+
+  def is_fizz?(number)
+    list.each do |number|
+      @number = number
+    end
+    number % 3 == 0
+  end
+
+  def is_buzz?(number)
+    list.each do |number|
+      @number = number
+    end
+    number % 5 == 0
   end
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,25 @@
 require './fizzbuzz.rb'
 require 'spec_helper'
 
-describe Fizzbuzz do
-  it 'should create an array from 1 to 100' do
-    game = Fizzbuzz.new
+describe Game do
+  it 'should create a list of numbers from 1 to 100' do
+    game = Game.new
     game.list.should include(1..100)
+  end
+
+  it 'should define "3" as "fizz"' do
+    game = Game.new
+    game.is_fizz?(3).should be_true
+  end
+
+  it 'should define "6" as "fizz' do
+    game = Game.new
+    game.is_fizz?(6).should be_true
+  end
+
+  it 'should define "5" as "buzz' do
+    game = Game.new
+    game.is_buzz?(5).should be_true
   end
 
 end
