d421d46650f430ca0c7ce1065a23e7578e47a06e
Neil Renicker <neilrenicker@gmail.com>
2013-02-27 11:27:35 -0500

    add and pass first specification

---
+++ fizzbuzz.rb
@@ -0,0 +1,7 @@
+class Fizzbuzz
+
+  def list
+    [1..100]
+  end
+
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,10 @@
-require "spec_helper"
+require './fizzbuzz.rb'
+require 'spec_helper'
+
+describe Fizzbuzz do
+  it 'should create an array from 1 to 100' do
+    game = Fizzbuzz.new
+    game.list.should include(1..100)
+  end
+
+end
