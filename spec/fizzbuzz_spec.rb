a43f3cbc7242a34cc0d6baba0ec3152957ea6722
Neil Renicker <neilrenicker@gmail.com>
2013-02-27 12:06:11 -0500

    test integer replacement

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,19 +7,30 @@ describe Game do
     game.list.should include(1..100)
   end
 
-  it 'should define "3" as "fizz"' do
+  it 'should know that "3" is "fizz"' do
     game = Game.new
     game.is_fizz?(3).should be_true
   end
 
-  it 'should define "6" as "fizz' do
+  it 'should know that "6" is "fizz"' do
     game = Game.new
     game.is_fizz?(6).should be_true
   end
 
-  it 'should define "5" as "buzz' do
+  it 'should know that "5" is "buzz"' do
     game = Game.new
     game.is_buzz?(5).should be_true
+  end
+
+  it 'should know that "25" is "buzz"' do
+    game = Game.new
+    game.is_buzz?(25).should be_true
+  end
+
+  xit 'should replace 3 with "fizz"' do
+    game = Game.new
+    game.printer.should include("fizz")
+    game.printer.should not_include("3")
   end
 
 end
