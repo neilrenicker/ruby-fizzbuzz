0bd08e1f52c09ab9c52720fee8961a1c5eca61e8
Neil Renicker <neilrenicker@gmail.com>
2013-03-05 12:20:04 -0500

    complete with working tests

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,49 @@
-list = (1..100).to_a
+require 'pry'
 
-list.each do |number|
+class Game
 
-  if number % 3 == 0 && number % 5 == 0
-    puts "FIZZBUZZ"
-  elsif number % 3 == 0
-    puts "fizz"
-  elsif number % 5 == 0
-    puts "buzz"
-  else
-    puts number
+  def is_fizz?(number)
+    number % 3 == 0
+  end
+
+  def is_buzz?(number)
+    number % 5 == 0
+  end
+
+  def run(list)
+    list.map do |number|
+      if is_fizz?(number) and is_buzz?(number)
+        "fizzbuzz"
+      elsif is_fizz?(number)
+        "fizz"
+      elsif is_buzz?(number)
+        "buzz"     
+      else
+        number
+      end
+    end
   end
 
 end
+
+# class Game
+
+#   list = (1..100).to_a
+
+#   list.each do |number|
+
+#     if number % 3 == 0 && number % 5 == 0
+#       puts "FIZZBUZZ"
+#     elsif number % 3 == 0
+#       puts "fizz"
+#     elsif number % 5 == 0
+#       puts "buzz"
+#     else
+#       puts number
+#     end
+#   end
+
+# end
 
 # class Game
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,13 +2,6 @@ require './fizzbuzz.rb'
 require 'spec_helper'
 
 describe Game do
-  it 'should create a list of numbers from 1 to 100' do
-    game = Game.new
-    game.list.should have_exactly(100).items
-    game.list.should start_with(1)
-    game.list.should end_with(100)
-  end
-
   it 'should know that numbers divisible by 3 are "fizz"' do
     game = Game.new
     game.is_fizz?(3).should be_true
@@ -23,9 +16,19 @@ describe Game do
     game.is_buzz?(25).should be_true
   end
 
-  xit 'should replace 3 with "fizz"' do
+  it 'should return "1, 2, fizz" for "1, 2, 3"' do
     game = Game.new
-    game.printer(3).should == "fizz"
+    game.run((1..3).to_a).should == [1, 2, "fizz"]
+  end
+
+  it 'should return "1, 2, fizz, 4, buzz" for "1, 2, 3, 4, 5"' do
+    game = Game.new
+    game.run((1..5).to_a).should == [1, 2, "fizz", 4, "buzz"]
+  end
+
+  it 'should return "14, fizzbuzz" for "14, 15"' do
+    game = Game.new
+    game.run((14..15).to_a).should == [14, "fizzbuzz"]
   end
 
 end
