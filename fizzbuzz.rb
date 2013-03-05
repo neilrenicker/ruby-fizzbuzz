9a6ffec690cadb10dc39b5a06945d5f1a1e4b1af
Neil Renicker <neilrenicker@gmail.com>
2013-03-05 10:52:26 -0500

    refactor game to run correctly, exclude tests

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,37 @@
-class Game
+list = (1..100).to_a
 
-  def list
-    [1..100]
-  end
+list.each do |number|
 
-  def is_fizz?(number)
-    list.each do |number|
-      @number = number
-    end
-    number % 3 == 0
-  end
-
-  def is_buzz?(number)
-    list.each do |number|
-      @number = number
-    end
-    number % 5 == 0
+  if number % 3 == 0 && number % 5 == 0
+    puts "FIZZBUZZ"
+  elsif number % 3 == 0
+    puts "fizz"
+  elsif number % 5 == 0
+    puts "buzz"
+  else
+    puts number
   end
 
 end
+
+# class Game
+
+#   def list
+#     @list = (1..100).to_a
+#   end
+
+#   def number
+#     list.each do |number|
+#       @number = number
+#     end
+#   end
+
+#   def is_fizz?(number)
+#     number % 3 == 0
+#   end
+
+#   def is_buzz?(number)
+#     number % 5 == 0
+#   end
+
+# end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,33 +4,28 @@ require 'spec_helper'
 describe Game do
   it 'should create a list of numbers from 1 to 100' do
     game = Game.new
-    game.list.should include(1..100)
+    game.list.should have_exactly(100).items
+    game.list.should start_with(1)
+    game.list.should end_with(100)
   end
 
-  it 'should know that "3" is "fizz"' do
+  it 'should know that numbers divisible by 3 are "fizz"' do
     game = Game.new
     game.is_fizz?(3).should be_true
-  end
-
-  it 'should know that "6" is "fizz"' do
-    game = Game.new
     game.is_fizz?(6).should be_true
+    game.is_fizz?(9).should be_true
   end
 
-  it 'should know that "5" is "buzz"' do
+  it 'should know numbers divisible by 5 are "buzz"' do
     game = Game.new
     game.is_buzz?(5).should be_true
-  end
-
-  it 'should know that "25" is "buzz"' do
-    game = Game.new
+    game.is_buzz?(10).should be_true
     game.is_buzz?(25).should be_true
   end
 
   xit 'should replace 3 with "fizz"' do
     game = Game.new
-    game.printer.should include("fizz")
-    game.printer.should not_include("3")
+    game.printer(3).should == "fizz"
   end
 
 end
